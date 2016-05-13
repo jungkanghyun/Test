89985be5002117cdd876689c2bcb74fd3a44717b
jkh120 <jkh120@nps-jkh120-m.ds.neowiz.com>
2016-05-13 17:57:04 +0900

    .

---
+++ SpringGradleDemo01/src/main/java/com/asuscomm/tutorial/IndexController.java
@@ -0,0 +1,23 @@
+package com.asuscomm.tutorial;
+
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+
+
+	@RequestMapping("/")
+	public String index(Model model) {
+
+		model.addAttribute("greeting","hello");
+		
+		return "index";
+	}
+}
---
+++ SpringGradleDemo01/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Gradle + Spring MVC</title>
+</head>
+<body>
+msg : ${greeting}
+</body>
+</html>
